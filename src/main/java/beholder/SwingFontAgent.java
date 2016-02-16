706e3d6303e05712f932e236c24097e6f7533b17
Roman Dawydkin <tanmatra@gmail.com>
2016-02-16 11:49:12 +0300

    Allow changing font size as well.

--- src/main/java/beholder/SwingFontAgent.java
+++ src/main/java/beholder/SwingFontAgent.java
@@ -9,14 +9,41 @@ public class SwingFontAgent
 {
     public static final String DEFAULT_FONT = "Segoe UI";
 
+    private static int oldFontSize;
+
+    private static int newFontSize;
+
     private static String fontName;
+
+    private static int parseInt(String string) {
+        try {
+            return Integer.parseInt(string);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
 
     @SuppressWarnings("unused")
     public static void premain(String args) {
         if ((args == null) || args.isEmpty()) {
             fontName = DEFAULT_FONT;
         } else {
-            fontName = args;
+            final int sizesIndex = args.indexOf(';');
+            if (sizesIndex < 0) {
+                fontName = args;
+            } else {
+                fontName = args.substring(0, sizesIndex);
+                final String sizesString = args.substring(sizesIndex + 1);
+                final int equalsIndex = sizesString.indexOf('=');
+                if (equalsIndex < 0) {
+                    newFontSize = parseInt(sizesString);
+                } else {
+                    final String oldSizeStr = sizesString.substring(0, equalsIndex);
+                    final String newSizeStr = sizesString.substring(equalsIndex + 1);
+                    oldFontSize = parseInt(oldSizeStr);
+                    newFontSize = parseInt(newSizeStr);
+                }
+            }
         }
         UIManager.addPropertyChangeListener(propertyChangeEvent -> {
             if ("lookAndFeel".equals(propertyChangeEvent.getPropertyName())) {
@@ -33,8 +60,13 @@ public class SwingFontAgent
             final Object value = uiDefaults.get(key);
             if (value instanceof FontUIResource) {
                 final FontUIResource oldFont = (FontUIResource) value;
-                final FontUIResource newFont =
-                        new FontUIResource(fontName, oldFont.getStyle(), oldFont.getSize());
+                int size = oldFont.getSize();
+                if (newFontSize != 0) {
+                    if ((oldFontSize == 0) || (oldFontSize == size)) {
+                        size = newFontSize;
+                    }
+                }
+                final FontUIResource newFont = new FontUIResource(fontName, oldFont.getStyle(), size);
                 uiDefaults.put(key, newFont);
             }
         }
